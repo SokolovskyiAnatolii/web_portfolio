359685fe60d051fd64e631fe9079d7c0c640a0f5
Anatolii Sokolovskyi <sans357@gmail.com>
2025-01-19 23:54:20 +0200

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,20 @@
 import "./styles/main.css";
 
-import instagram from "./img/icons/instagram.svg";
-import twitter from "./img/icons/twitter.svg";
-import gitHub from "./img/icons/gitHub.svg";
-import linkedIn from "./img/icons/linkedIn.svg";
 
-import Navbar from "./components/navbar/navbar";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+
 
 function App() {
   return (
     <>
       <Navbar />
 
-      <header className="header">
-        <div className="header__wrapper">
-          <h1 className="header__title">
-            <strong>
-              Hi, my name is <em>Anatolii</em>
-            </strong>
-            <br />a frontend developer
-          </h1>
-          <div className="header__text">
-            <p>with passion for learning and creating.</p>
-          </div>
-          <a href="#!" className="btn">
-            Download CV
-          </a>
-        </div>
-      </header>
+<Home />
 
-      <main className="section">
-        <div className="container">
-          <ul className="content-list">
-            <li className="content-list__item">
-              <h2 className="title-2">Frontend</h2>
-              <p>
-                JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM,
-                BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
-              </p>
-            </li>
-            <li className="content-list__item">
-              <h2 className="title-2">Backend</h2>
-              <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
-            </li>
-          </ul>
-        </div>
-      </main>
-
-      <footer className="footer">
-        <div className="container">
-          <div className="footer__wrapper">
-            <ul className="social">
-              <li className="social__item">
-                <a href="#!">
-                  <img src={instagram} alt="Link" />
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={twitter} alt="Link" />
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={gitHub} alt="Link" />
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={linkedIn} alt="Link" />
-                </a>
-              </li>
-            </ul>
-            <div className="copyright">
-              <p>© 2022 frontend-dev.com</p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer />
+      
     </>
   );
 }
